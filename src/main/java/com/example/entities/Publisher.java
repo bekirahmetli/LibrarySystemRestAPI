a2ef0522cf75786abcabe3b452000814d9abec31
bekirahmetli <bekirahmetli1905@gmail.com>
2025-10-16 15:22:09 +0300

    feat(publisher-entity): add Publisher entity

---
+++ src/main/java/com/example/entities/Publisher.java
@@ -0,0 +1,34 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "publishers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "publisher_id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "publisher_name")
+    private String publisherName;
+
+    @Column(name = "establishment_year")
+    private int establishmentYear;
+
+    @Column(name = "address")
+    private String address;
+
+    @OneToMany(mappedBy = "publisher",cascade = {CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},fetch = FetchType.LAZY)
+    private List<Book> books;
+}
