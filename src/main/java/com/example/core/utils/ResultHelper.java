8f623892449f3f65eb33447e07d2836a6b4c7d9c
bekirahmetli <bekirahmetli1905@gmail.com>
2025-10-17 21:11:47 +0300

    feat(core-utils): add success method in ResultHelper for standardized 200 responses

--- src/main/java/com/example/core/utils/ResultHelper.java
+++ src/main/java/com/example/core/utils/ResultHelper.java
@@ -3,7 +3,12 @@ package com.example.core.utils;
 import com.example.core.result.ResultData;
 
 public class ResultHelper {
+
     public static <T>ResultData<T> created(T data){
         return new ResultData<>(true, Message.CREATED,"201",data);
+    }
+
+    public static <T> ResultData<T> success(T data){
+        return new ResultData<>(true,Message.SUCCESS,"200",data);
     }
 }
