afb4baa4adb9277f4abe76734a205556894c660c
bekirahmetli <bekirahmetli1905@gmail.com>
2025-10-17 17:45:37 +0300

    feat(author-dto): add AuthorResponse DTO for returning author data

---
+++ src/main/java/com/example/dto/response/AuthorResponse.java
@@ -0,0 +1,24 @@
+package com.example.dto.response;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorResponse {
+    @Positive
+    @NotNull
+    private int id;
+    @NotNull
+    private String  authorName;
+    @NotNull
+    private LocalDate birthDate;
+    @NotNull
+    private String country;
+}
