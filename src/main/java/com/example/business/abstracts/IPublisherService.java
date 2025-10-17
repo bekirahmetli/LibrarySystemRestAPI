97e5a57956fcfd765975199a871e5445ad553e42
bekirahmetli <bekirahmetli1905@gmail.com>
2025-10-18 02:43:58 +0300

    feat(service): add IPublisherService interface

---
+++ src/main/java/com/example/business/abstracts/IPublisherService.java
@@ -0,0 +1,12 @@
+package com.example.business.abstracts;
+
+import com.example.entities.Publisher;
+import org.springframework.data.domain.Page;
+
+public interface IPublisherService {
+    Publisher save(Publisher publisher);
+    Publisher get(int id);
+    Publisher update(Publisher publisher);
+    boolean delete(int id);
+    Page<Publisher> cursor(int page, int pageSize);
+}
