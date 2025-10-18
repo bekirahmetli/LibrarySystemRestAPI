99618819a4fcdf4bf1c9fcf3656d4064b738c29a
bekirahmetli <bekirahmetli1905@gmail.com>
2025-10-18 20:27:06 +0300

    feat(controller): add save endpoint to BookController

---
+++ src/main/java/com/example/api/BookController.java
@@ -0,0 +1,32 @@
+package com.example.api;
+
+import com.example.business.abstracts.IBookService;
+import com.example.core.config.modelMapper.IModelMapperService;
+import com.example.core.result.ResultData;
+import com.example.core.utils.ResultHelper;
+import com.example.dto.request.book.BookSaveRequest;
+import com.example.dto.response.book.BookResponse;
+import com.example.entities.Book;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/books")
+public class BookController {
+    private final IBookService bookService;
+    private final IModelMapperService modelMapperService;
+
+    public BookController(IBookService bookService, IModelMapperService modelMapperService) {
+        this.bookService = bookService;
+        this.modelMapperService = modelMapperService;
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResultData<BookResponse> save(@Valid @RequestBody BookSaveRequest bookSaveRequest){
+        Book saved = bookService.save(bookSaveRequest);
+        BookResponse response = modelMapperService.forResponse().map(saved, BookResponse.class);
+        return ResultHelper.created(response);
+    }
+}
