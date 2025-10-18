00d149924382a13c728d643d83269c3be0f76453
bekirahmetli <bekirahmetli1905@gmail.com>
2025-10-18 19:27:52 +0300

    feat(repository): add BookRepo with existsByCategories_Id method

---
+++ src/main/java/com/example/dao/BookRepo.java
@@ -0,0 +1,10 @@
+package com.example.dao;
+
+import com.example.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepo extends JpaRepository<Book,Integer> {
+    boolean existsByCategories_Id(int categoryId);
+}
