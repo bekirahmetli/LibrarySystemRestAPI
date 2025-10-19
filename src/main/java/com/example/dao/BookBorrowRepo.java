4a15278877a4f68d920e8df84b32339d1250ccf4
bekirahmetli <bekirahmetli1905@gmail.com>
2025-10-19 17:43:03 +0300

    feat(repository): add BookBorrowRepo for BookBorrowing entity

---
+++ src/main/java/com/example/dao/BookBorrowRepo.java
@@ -0,0 +1,9 @@
+package com.example.dao;
+
+import com.example.entities.BookBorrowing;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookBorrowRepo extends JpaRepository<BookBorrowing,Integer> {
+}
