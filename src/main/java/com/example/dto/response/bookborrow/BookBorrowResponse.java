254a5ed90dbc775464f80aeaccdbf3887f1e48f4
bekirahmetli <bekirahmetli1905@gmail.com>
2025-10-19 17:55:31 +0300

    feat(dto): add BookBorrowResponse for returning book borrow data

---
+++ src/main/java/com/example/dto/response/bookborrow/BookBorrowResponse.java
@@ -0,0 +1,19 @@
+package com.example.dto.response.bookborrow;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookBorrowResponse {
+    private int id;
+    private String borrowerName;
+    private String borrowerEmail;
+    private LocalDate borrowingDate;
+    private LocalDate returnDate;
+    private int bookId;
+}
